630e484bb0736e9eade7a08162b2273a7b8d2a52
Roberto Polli <robipolli@gmail.com>
2022-06-22 19:37:41 +0200

    nesting

--- test_oasld.py
+++ test_oasld.py
@@ -7,13 +7,7 @@ from typing import Dict
 import jsonschema
 import yaml
 
-log = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-schema = yaml.safe_load(Path("schemas.yaml").read_text())
-
-person = schema["Person"]
-EXAMPLE = schema["Person"]["example"]
+CTX = "@context"
 
 
 class RefResolver:
@@ -26,27 +20,34 @@ class RefResolver:
 
 
 class Instance:
+    NO_CONTEXT = object()
+
     def __init__(self, instance, schema, context=None, is_subentry=False) -> None:
         self.json_instance = instance
         self.schema = schema
         self.ld = deepcopy(instance) if not is_subentry else instance
-        self.is_subentry = False
-        self.subentry_context_ref = context
-        self.jcontext = schema.get("x-jsonld-context", {})
+        self.is_subentry = is_subentry
         if jtype := schema.get("x-jsonld-type"):
             self.ld["@type"] = jtype
+
+        self.jcontext = schema.get("x-jsonld-context", {})
+
+        if context is Instance.NO_CONTEXT:
+            # Explicitly skipping context merge.
+            self.subentry_context_ref = {CTX: {}}
+            return
+        self.subentry_context_ref = context
         if self.jcontext:
             if not self.is_subentry:
-                self.ld["@context"] = deepcopy(self.jcontext)
-            elif self.subentry_context_ref:
-                if "@context" in self.subentry_context_ref:
-                    raise ValueError(
-                        "Cannot overwrite a @context defined in the super-schema"
-                    )
-                self.subentry_context_ref["@context"] = deepcopy(self.jcontext)
-            elif self.subentry_context_ref is None:
-                # Explicitly skipping context merge.
-                pass
+                # Initialize.
+                self.subentry_context_ref = {CTX: deepcopy(self.jcontext)}
+            elif CTX in context:
+                raise ValueError(
+                    "Cannot overwrite a @context defined in the super-schema"
+                )
+            elif context:
+                # Merge in the passed context
+                self.subentry_context_ref[CTX] = deepcopy(self.jcontext)
             else:
                 raise NotImplementedError("An sub-entry MUST have a context")
 
@@ -54,8 +55,10 @@ class Instance:
         properties = self.schema["properties"]
         for k, v in self.ld.items():
             process_keywords = {"@context", "@type"} - set(self.jcontext.get(k, {}))
-            log.info(f"Looking for {process_keywords}")
             property_schema = properties.get(k, {})
+            log.warn(f"Looking for {process_keywords} on {k} => {property_schema}")
+            if schema_ref := property_schema.get("$ref"):
+                property_schema = resolver.resolve(schema_ref.strip("#"))
             if all(
                 (property_schema.get("type") in ("object", "array"), process_keywords)
             ):
@@ -64,8 +67,9 @@ class Instance:
                     subschema = resolver.resolve(
                         property_schema["items"]["$ref"].strip("#")
                     )
-                    subcontext = self.ld["@context"][k]
-                    for subinstance in v:
+                    subcontext = self.subentry_context_ref[CTX][k]
+                    for idx, subinstance in enumerate(v):
+                        log.debug(f"Integrating context id {id(subcontext)}")
                         i = Instance(
                             subinstance, subschema, context=subcontext, is_subentry=True
                         )
@@ -74,102 +78,120 @@ class Instance:
                         # This can be somewhat limitative because the traversing process
                         # depends on the subinstance properties and not on the ones
                         # of the schema.
-                        subcontext = None
+                        subcontext = Instance.NO_CONTEXT
                 elif property_schema["type"] == "object":
-                    process_object()
+                    subschema = property_schema
+                    subcontext = self.subentry_context_ref.setdefault(k, {})
+                    # if k == "spouse": import pdb; pdb.set_trace()
+                    i = Instance(v, subschema, context=subcontext, is_subentry=True)
+                    i.process_instance(resolver)
                 else:
                     raise NotImplementedError
+        if not self.is_subentry:
+            self.ld[CTX] = self.subentry_context_ref[CTX]
 
 
-def process_instance(instance, schema, schemas=None, is_subentry=False, context=None):
-    schemas = schemas or {}
-
-    schema_resolver = jsonschema.RefResolver("/", "")
-    if jtype := schema.get("x-jsonld-type"):
-        instance["@type"] = jtype
-    if jcontext := schema.get("x-jsonld-context"):
-        if not is_subentry:
-            instance["@context"] = deepcopy(jcontext)
-        elif context:
-            if "@context" in context:
-                raise ValueError(
-                    "Cannot overwrite a @context defined in the super-schema"
-                )
-            context["@context"] = deepcopy(jcontext)
-        elif context is None:
-            pass
-        else:
-            raise NotImplementedError("An sub-entry MUST have a context")
-
-    properties = schema["properties"]
-    for k, v in instance.items():
-        process_keywords = {"@context", "@type"} - set(jcontext.get(k, {}))
-        log.info(f"Looking for {process_keywords}")
-        property_schema = properties.get(k, {})
-        if all((property_schema.get("type") in ("object", "array"), process_keywords)):
-            log.info(k, v)
-            if property_schema["type"] == "array":
-                subschema = schema_resolver.resolve_fragment(
-                    schemas, property_schema["items"]["$ref"].strip("#")
-                )
-                subcontext = instance["@context"][k]
-                for subinstance in v:
-                    process_instance(
-                        subinstance,
-                        subschema,
-                        schemas,
-                        is_subentry=True,
-                        context=subcontext,
-                    )
-                    # Only merge context for the first processing entry.
-                    # This can be somewhat limitative because the traversing process
-                    # depends on the subinstance properties and not on the ones
-                    # of the schema.
-                    subcontext = None
-            elif property_schema["type"] == "object":
-                process_object()
-            else:
-                raise NotImplementedError
-
-    return instance
+import pytest
 
 
-def test_process_array():
-    context = {
-        "@vocab": "https://w3.org/ns/person#",
-        "children": {"@container": "@set"},
-        "email": "@id",
-    }
-    actual = process_instance(
-        instance={"email": "a@b.c"},
-        schema={
-            "properties": {
-                "children": {"items": {"$ref": "#/Person"}, "type": "array"},
-                "email": {"type": "string"},
+@pytest.fixture
+def resolver(schema_yaml):
+    return RefResolver(schema_yaml)
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+@pytest.fixture
+def schema_yaml():
+    return yaml.safe_load(Path("schemas.yaml").read_text())
+
+
+@pytest.mark.parametrize(
+    "testcase,expected",
+    [
+        (
+            dict(
+                instance={},
+                schema={"x-jsonld-context": "http://foo.example", "type": "object"},
+                context=Instance.NO_CONTEXT,
+            ),
+            {},
+        ),
+        (
+            dict(
+                instance={},
+                schema={"x-jsonld-context": "http://foo.example", "type": "object"},
+            ),
+            "http://foo.example",
+        ),
+        (
+            dict(
+                instance={},
+                schema={"x-jsonld-context": "http://foo.example", "type": "object"},
+                context={"foo": "bar"},
+            ),
+            "http://foo.example",
+        ),
+        (
+            dict(
+                instance={"givenName": "Mario"},
+                schema={"x-jsonld-context": "http://foo.example", "type": "object"},
+                context={"@container": "@set"},
+                is_subentry=True,
+            ),
+            {
+                "@container": "@set",
+                "@context": "http://foo.example",
             },
-            "x-jsonld-context": {
-                "@vocab": "https://w3.org/ns/person#",
-                "children": {"@container": "@set"},
-                "email": "@id",
-            },
-            "x-jsonld-type": "Person",
-        },
-        schemas={},
-        is_subentry=True,
-        context=context["children"],
+        ),
+        (
+            dict(
+                instance={"foo": 1, "biz": 2},
+                schema={"x-jsonld-context": {"biz": "pop"}, "type": "object"},
+                context={"@container": "@set"},
+                is_subentry=True,
+            ),
+            {"@container": "@set", "@context": {"biz": "pop"}},
+        ),
+    ],
+)
+def test_init(testcase, expected):
+    context = testcase.pop("context", None)
+    i = Instance(
+        **testcase,
+        context=deepcopy(context) if context is not Instance.NO_CONTEXT else context,
     )
-    assert context["children"]["@context"]["@vocab"]
+    res = (
+        i.subentry_context_ref
+        if i.is_subentry
+        else i.subentry_context_ref.get("@context")
+    )
+    assert res == expected
 
 
-def test_instance():
-    resolver = RefResolver(schema)
-    instance = Instance(EXAMPLE, person)
-    instance.process_instance(resolver=resolver)
-    print(json.dumps(instance.ld))
+def test_person(resolver, schema_yaml):
+    schema = schema_yaml["Person"]
+    instance = harn_schema(schema, resolver)
+    json.dump(instance.ld, fp=open("tmp.person.json", "w"), indent=2)
 
 
-def test_example():
-    example = dict(**EXAMPLE)
-    actual = process_instance(example, person, schema)
-    print(json.dumps(actual))
+def test_granny(resolver, schema_yaml):
+    schema = schema_yaml["Granny"]
+    instance = harn_schema(schema, resolver)
+    json.dump(instance.ld, fp=open("tmp.granny.json", "w"), indent=2)
+
+
+def test_spouse(resolver, schema_yaml):
+    schema = schema_yaml["Spouse"]
+    harn_schema(schema, resolver)
     raise NotImplementedError
+
+
+def harn_schema(schema, resolver):
+    example = schema["example"]
+    instance = Instance(example, schema)
+    instance.process_instance(resolver=resolver)
+    log.info("\n" + json.dumps(instance.ld))
+    return instance
